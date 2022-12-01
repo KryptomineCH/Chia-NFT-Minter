a34c91487c36226f24aeed8a6f57d322057ed24d
Julian B <bechti44@googlemail.com>
2022-12-01 14:35:39 +0100

    modified directories to be public

--- Chia-NFT-Minter/Directories.cs
+++ Chia-NFT-Minter/Directories.cs
@@ -6,29 +6,33 @@ using System.Threading.Tasks;
 
 namespace Chia_NFT_Minter
 {
-    internal static class Directories
+    public static class Directories
     {
+        static Directories()
+        {
+            InitializeDirectories();
+        }
         /// <summary>
         /// this directory stores the NFT files. Eg, the artwork, image, video or document
         /// </summary>
-        internal static DirectoryInfo Nfts = new DirectoryInfo("final");
+        public static DirectoryInfo Nfts = new DirectoryInfo("final");
         /// <summary>
         /// this directory stores the preview images for the nfts.
         /// </summary>
-        internal static DirectoryInfo Preview = new DirectoryInfo("preview");
+        public static DirectoryInfo Preview = new DirectoryInfo("preview");
         /// <summary>
         /// this directory stores the Metadata json files for the NFTs
         /// </summary>
-        internal static DirectoryInfo Metadata = new DirectoryInfo("metadata");
+        public static DirectoryInfo Metadata = new DirectoryInfo("metadata");
         /// <summary>
         /// this directory stores the metadata json files which are used to describe the nft and its properties
         /// </summary>
-        internal static DirectoryInfo Rpcs = new DirectoryInfo("rpcs");
+        public static DirectoryInfo Rpcs = new DirectoryInfo("rpcs");
         /// <summary>
         /// the offers directory can be used to store offer files
         /// </summary>
-        internal static DirectoryInfo Offers = new DirectoryInfo("offers");
-        internal static void InitializeDirectories()
+        public static DirectoryInfo Offers = new DirectoryInfo("offers");
+        private static void InitializeDirectories()
         {
             if (!Nfts.Exists) Nfts.Create();
             if (!Preview.Exists) Preview.Create();
