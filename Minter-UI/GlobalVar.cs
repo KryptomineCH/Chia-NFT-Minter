33875d72ff5b8f0eb70703ffc8549ee2e5c218a5
Julian B <bechti44@googlemail.com>
2022-12-03 22:19:41 +0100

    added globalvar

---
+++ Minter-UI/GlobalVar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minter_UI
+{
+    internal static class GlobalVar
+    {
+        internal static bool CaseSensitiveFilehandling = true;
+    }
+}
