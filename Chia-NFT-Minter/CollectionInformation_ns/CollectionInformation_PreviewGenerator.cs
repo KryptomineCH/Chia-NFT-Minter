4ddc4e1e904a666fec3827622a8fceb5b48076d0
Julian B <bechti44@googlemail.com>
2023-01-15 13:48:07 +0100

    fixed preview generation blocking ui and memory issues

--- Chia-NFT-Minter/CollectionInformation_ns/CollectionInformation_PreviewGenerator.cs
+++ Chia-NFT-Minter/CollectionInformation_ns/CollectionInformation_PreviewGenerator.cs
@@ -1,13 +1,26 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
+using System.Windows.Controls;
 
 namespace Chia_NFT_Minter.CollectionInformation_ns
 {
     public static partial class CollectionInformation
     {
-        private static void GeneratePreviews(bool caseSensitive, CollectionInformation_Object newInfo)
+        private static volatile bool PreviewGenerationRunning = false;
+        private static object PreviewGenerationLock = new object();
+        private static async Task GeneratePreviews(bool caseSensitive, CollectionInformation_Object newInfo)
         {
-            foreach(FileInfo info in newInfo.NftFileInfos)
+            lock (PreviewGenerationLock)
+            {
+                if (PreviewGenerationRunning) return;
+                PreviewGenerationRunning = true;
+            }
+            Bitmap bitmap = new Bitmap(1000,1000);
+            Bitmap preview = new Bitmap(250,250);
+            ImageCodecInfo jpegCodec = ImageCodecInfo.GetImageEncoders().First(c => c.FormatID == ImageFormat.Jpeg.Guid);
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = new EncoderParameter(Encoder.Quality, 70L);
+            foreach (FileInfo info in newInfo.NftFileInfos)
             {
                 if (info.Extension == ".png" || info.Extension == ".jpg" || info.Extension == ".tiff")
                 {
@@ -21,14 +34,45 @@ namespace Chia_NFT_Minter.CollectionInformation_ns
                     FileInfo previewFile = new FileInfo(Path.Combine(Directories.Preview.FullName, name + ".jpg"));
                     if (!previewFile.Exists)
                     {
-                        Bitmap bitmap = new Bitmap(info.FullName);
-                        Bitmap preview = BitmapHelper_Net.BitmapConverter.ResizeLongEdge(ref bitmap,300);
-                        preview.Save(previewFile.FullName, ImageFormat.Jpeg);
+                        FileInfo previewOrigFile = new FileInfo(Path.Combine(Directories.Preview.FullName, name + ".png"));
+                        if (previewOrigFile.Exists)
+                        {
+                            newInfo.NftPreviewFiles[key] = previewOrigFile;
+                            continue;
+                        }
+                        using (bitmap = new Bitmap(info.FullName))
+                        {
+                            using (preview = BitmapHelper_Net.BitmapConverter.ResizeLongEdge(ref bitmap, 250))
+                            {
+                                // save preview to memory to compare size.
+                                // If larger than original, use orig as preview
+                                using (MemoryStream memoryStream = new MemoryStream())
+                                {
+                                    preview.Save(memoryStream, ImageFormat.Jpeg);
+                                    long previewSize = memoryStream.Length;
+                                    long origSize = info.Length;
+                                    if (previewSize * 1.2 > origSize)
+                                    { // use original image as preview
+                                        info.CopyTo(previewOrigFile.FullName);
+                                        newInfo.NftPreviewFiles[key] = previewOrigFile;
+                                    }
+                                    else
+                                    { // save preview png
+                                        preview.Save(previewFile.FullName, jpegCodec, encoderParams);
+                                        newInfo.NftPreviewFiles[key] = previewFile;
+                                    }
+                                }
+                            }
+                        }
                     }
-                    newInfo.NftPreviewFiles[key] = previewFile;
+                    else {
+                        newInfo.NftPreviewFiles[key] = previewFile;
+                    }
                 }
             }
-
+            preview.Dispose();
+            bitmap.Dispose();
+            PreviewGenerationRunning = false;
         }
     }
 }
